bea0b8d8d3bdd33a9f8fc975174f3fe1c8ef14de
Yael-Karat <yaelkarat01@gmail.com>
2025-06-23 19:20:46 +0300

    change init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.models import User  # חשוב לייבא את המודל
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -13,6 +12,7 @@ login.login_view = "login"
 
 @login.user_loader
 def load_user(user_id):
+    from app.models import User
     return User.query.get(int(user_id))
 
 
