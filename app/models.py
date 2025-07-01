748ea7e0f02f51014ab6f3d81bb0bac28ffdaea5
Yael-Karat <yaelkarat01@gmail.com>
2025-07-01 18:29:27 +0300

    Added cascade="all, delete-orphan" to automatically handle deleting lists and items when the user is deleted.

--- app/models.py
+++ app/models.py
@@ -6,14 +6,14 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    lists = db.relationship("ShoppingList", backref="user", lazy=True)
+    lists = db.relationship("ShoppingList", backref="user", lazy=True, cascade="all, delete-orphan")
 
 
 class ShoppingList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    items = db.relationship("Item", backref="list", lazy=True)
+    items = db.relationship("Item", backref="list", lazy=True, cascade="all, delete-orphan")
 
 
 class Item(db.Model):
